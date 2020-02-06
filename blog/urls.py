4e9f3553bdc1b6ff9721ec33218f73ac0007468b
jiajunchang <jiajunc@uci.edu>
2020-02-06 04:40:39 -0800

    Add blog page

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = "blog"
 urlpatterns = [
     # post views
-    path("", views.get_post_list, name = "get_post_list")
+    path("", views.get_post_list, name = "get_post_list"),
+    path(
+        "<int:year>/<int:month>/<int:day>/<str:slug>",
+        views.get_single_post,
+        name = "get_single_post"
+    )
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,16 @@ def get_post_list(request):
         "blog/post/list.html",
         {"post_list": posts}
     )
+
+def get_single_post(request, year, month, day, slug):
+    post = Post.objects.filter(
+        publish__year = year,
+        publish__month = month,
+        publish__day = day,
+        slug = slug
+    ).first()
+    return render(
+        request,
+        "blog/post/blog_page.html",
+        {"post": post}
+    )
