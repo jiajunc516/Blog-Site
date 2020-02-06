5832f25a71b5ca2cdceffa9603bfb963d4f3e558
jiajunchang <jiajunc@uci.edu>
2020-02-06 04:57:21 -0800

    Add link to blog page

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -33,3 +34,14 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+    def get_url(self):
+        return reverse(
+            "blog:single_post",
+            args = [
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
