f096801f17405ce767dfd9ec439f0cb013f1a287
jiajunchang <jiajunc@uci.edu>
2020-02-07 04:51:19 -0800

    Add tags

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class Post(models.Model):
@@ -28,6 +29,7 @@ class Post(models.Model):
         max_length=10,
         choices=STATUS_CHOICES,
         default=DRAFT)
+    tags = TaggableManager()
     
     class Meta:
         ordering = ("-publish",)
