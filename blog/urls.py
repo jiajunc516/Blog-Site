71af455757ac5c448802d15a0bdb749b75fec0a7
jiajunchang <jiajunc@uci.edu>
2020-02-09 01:56:59 -0800

    Add similar posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,7 @@ app_name = "blog"
 urlpatterns = [
     # post views
     path("", views.get_post_list, name = "post_list"),
-    path(
-        "<int:year>/<int:month>/<int:day>/<str:slug>/",
+    path("<int:year>/<int:month>/<int:day>/<str:slug>/",
         views.get_single_post,
         name = "single_post"),
     path("tag/<str:tag_slug>/",
